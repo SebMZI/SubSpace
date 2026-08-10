ec08f8597548c373cccda25b7bff9247c4bf03d7
SebMZI <sebastien.mzi@proton.me>
2026-08-10 23:06:05 +0200

    feat: implement TagsController to dispatch methods to correct service

---
+++ backend/src/controllers/tags.controller.ts
@@ -0,0 +1,49 @@
+import {TagsService} from "../services/tags.service";
+
+export class TagsController {
+    constructor(private tagsService: TagsService) {}
+
+    async getTags(req: any, res: any) {
+        try {
+            const RESULT = await this.tagsService.getTags(req);
+            res.status(200).send(RESULT);
+        } catch (e) {
+            res.status(500).send({
+                message: "Failed to get all tags",
+            })
+        }
+    }
+
+    async addTag(req: any, res: any) {
+        try {
+            const RESULT = await this.tagsService.addTag(req);
+            res.status(200).send(RESULT);
+        } catch (e) {
+            res.status(500).send({
+                message: "Failed to add tag",
+            })
+        }
+    }
+
+    async editTag(req: any, res: any) {
+        try {
+            const RESULT = await this.tagsService.editTag(req);
+            res.status(200).send(RESULT);
+        } catch (e) {
+            res.status(500).send({
+                message: "Failed to modify tag",
+            })
+        }
+    }
+
+    async deleteTag(req: any, res: any) {
+        try {
+            const RESULT = await this.tagsService.deleteTag(req);
+            res.status(200).send(RESULT);
+        } catch (e) {
+            res.status(500).send({
+                message: "Failed to delete tag",
+            })
+        }
+    }
+}
