56e6950097e312ae9e4523c09cc2003059f3590d
SebMZI <sebastien.mzi@proton.me>
2026-08-10 23:07:48 +0200

    feat: add routes to main app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,15 +3,19 @@ import express from "express";
 import {connectDatabase} from "./config/db.config";
 import "./models/associations";
 import AUTH_ROUTER from "./routes/auth.route";
+import TAGS_ROUTER from "./routes/tag.route";
 
-const app = express();
-const port = process.env.PORT || 3000;
+const APP = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+APP.use(express.json());
 
-app.use("/auth", AUTH_ROUTER)
+// ROUTES
+APP.use("/auth", AUTH_ROUTER);
+APP.use("/tags", TAGS_ROUTER);
 
-app.listen(port, async () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// SERVER
+APP.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
     await connectDatabase();
 });
