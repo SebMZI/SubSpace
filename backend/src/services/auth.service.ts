aeca1cb519d3efd2054bf3ca9ff3f50f6d93b1d5
SebMZI <sebastien.mzi@proton.me>
2026-08-10 21:09:25 +0200

    feat: implement authService class that manage the business part of the authentication

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,147 @@
+import User from "../models/user.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+interface Register {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+}
+
+interface Login {
+    email: string;
+    password: string;
+}
+
+
+export class AuthService {
+    async signin(body: Login) {
+        try {
+            const { email, password } = body;
+            this.validateEmail(email);
+            this.validatePassword(password);
+
+            const user = await User.findOne({where: {email}});
+            if(!user) {
+                throw new Error("Username or password invalid");
+            }
+
+            const hashedPassword = user.get("password") as string;
+            const isMatch = this.isPasswordAMatch(password, hashedPassword ?? null)
+            if(!isMatch) {
+                throw new Error("Username or password invalid");
+            }
+
+            const userData = user.toJSON();
+            delete userData.password;
+
+            const token = this.generateToken(userData);
+
+            return {
+                message: "Sign in successfully",
+                data: {
+                    token,
+                    user: userData
+                }
+            }
+        } catch (e) {
+           throw e
+        }
+    }
+
+    async signup(body: Register) {
+        try {
+            const { email, password, firstName, lastName } = body;
+            this.validateEmail(email);
+            this.validatePassword(password);
+            this.validateFirstName(firstName);
+            this.validateLastName(lastName);
+
+            const hashedPassword = await this.generateHashedPassword(password);
+            const newUser = await User.create({
+                email,
+                password: hashedPassword,
+                firstName,
+                lastName
+            })
+
+            await newUser.save();
+            const userData = await newUser.toJSON();
+            delete userData.password;
+            return {
+                message: "Sign up successfully",
+                data: {
+                    user: userData
+                }
+            };
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    private validateEmail(email: string) {
+        if(!email) {
+            throw new Error("email address is required");
+        }
+
+        if(!email.match("^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$")) {
+            throw new Error("Invalid email address");
+        }
+    }
+
+    private validatePassword(password: string) {
+        if(!password || password.length < 8) {
+            throw new Error("Password must be at least 8 characters");
+        }
+    }
+
+    private validateFirstName(firstName: string) {
+        if(!firstName) {
+            throw new Error("First name is required");
+        }
+    }
+
+    private validateLastName(lastName: string) {
+        if(!lastName) {
+            throw new Error("Last name is required");
+        }
+    }
+
+    private async generateHashedPassword(password: string) {
+        if(!password) {
+            throw new Error("Password must be at least 8 characters");
+        }
+
+        try {
+            const salt = bcrypt.genSaltSync(Number(process.env.PWD_SALT ?? 10));
+            return await bcrypt.hash(password, salt)
+        }catch (e) {
+            throw e
+        }
+    }
+
+    private async isPasswordAMatch(password: string, hashedPassword: string) {
+        if(!hashedPassword || !password) {
+            throw new Error("Password not found");
+        }
+
+        return await bcrypt.compare(password, hashedPassword);
+    }
+
+    private generateToken(user: any) {
+        const secret = process.env.JWT_SECRET;
+        if(!secret) {
+            throw new Error("ENV JWT_SECRET IS NOT DECLARED");
+        }
+
+        const expiresIn = process.env.JWT_EXPIRES_IN;
+        if(!expiresIn) {
+            throw new Error("JWT_EXPIRES_IN is required");
+        }
+
+        return jwt.sign({
+            data: user.userId
+        }, secret, {expiresIn: expiresIn as jwt.SignOptions["expiresIn"]})
+    }
+}
