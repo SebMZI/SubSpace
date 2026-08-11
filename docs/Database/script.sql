eb9f1cd0c125e12cf201cba2b99769110887cd4b
SebMZI <sebastien.mzi@proton.me>
2026-08-11 11:17:01 +0200

    fix: content table - Remove lastUpdated column

--- docs/Database/script.sql
+++ docs/Database/script.sql
@@ -3,9 +3,7 @@ CREATE TABLE IF NOT EXISTS "Users" (
     "email" VARCHAR(255) NOT NULL UNIQUE,
     "firstName" VARCHAR(150) NOT NULL,
     "lastName" VARCHAR(150) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP NOT NULL,
-    "updatedAt" TIMESTAMP NOT NULL
+    "password" VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "Contents"  (
@@ -13,8 +11,6 @@ CREATE TABLE IF NOT EXISTS "Contents"  (
     title VARCHAR(100) NOT NULL,
     description TEXT,
     "thumbnailURL" VARCHAR(500) NOT NULL,
-    "createdAt" DATE NOT NULL,
-    "updatedAt" DATE NOT NULL,
     duration INT NULL, -- Video Class
     "channelId" INT NULL, -- Video Class
     "subscriberCount" INT NULL, -- Channel Class
@@ -26,16 +22,12 @@ CREATE TABLE IF NOT EXISTS "Tags" (
     "tagId" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     label VARCHAR(50) NOT NULL,
     "userId" INT NOT NULL,
-    "createdAt" DATE NOT NULL,
-    "updatedAt" DATE NOT NULL,
     CONSTRAINT fk_tags_user FOREIGN KEY ("userId") REFERENCES "Users"("userId")
 );
 
 CREATE TABLE IF NOT EXISTS "ContentTags" (
     "tagId" INT NOT NULL,
     "contentId" INT NOT NULL,
-    "createdAt" DATE NOT NULL,
-    "updatedAt" DATE NOT NULL,
     CONSTRAINT pk_contenttags_ids PRIMARY KEY ("tagId", "contentId"),
     CONSTRAINT fk_contenttags_tagId FOREIGN KEY ("tagId") REFERENCES "Tags"("tagId"),
     CONSTRAINT fk_contenttags_contentId FOREIGN KEY ("contentId") REFERENCES "Contents"("contentId")
