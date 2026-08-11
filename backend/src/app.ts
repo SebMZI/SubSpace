4026adf8c368801839333c10a2ff217731b1df52
SebMZI <sebastien.mzi@proton.me>
2026-08-11 12:39:49 +0200

    feat: enable middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,8 @@
 import "./config/env.config";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import {rateLimit} from "express-rate-limit";
 import {connectDatabase} from "./config/db.config";
 import "./models/associations";
 import AUTH_ROUTER from "./routes/auth.route";
@@ -9,13 +12,25 @@ import CONTENT_ROUTER from "./routes/content.route";
 const APP = express();
 const PORT = process.env.PORT || 3000;
 
+const GLOBAL_LIMITER = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    limit: 300,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+});
+
+APP.use(cors());
+APP.use(helmet());
+APP.use(GLOBAL_LIMITER);
 APP.use(express.json());
 
 // ROUTES
 APP.use("/auth", AUTH_ROUTER);
 APP.use("/tags", TAGS_ROUTER);
 APP.use("/contents", CONTENT_ROUTER);
-
+APP.use((req: any, res: any)=> {
+    res.status(404).send("Not Found");
+})
 // SERVER
 APP.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
