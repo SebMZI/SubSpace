f450a2c329e7c4cb2817b3906b0b37294ff3e729
SebMZI <sebastien.mzi@proton.me>
2026-08-11 11:46:59 +0200

    fix: script.sql

--- docs/Database/script.sql
+++ docs/Database/script.sql
@@ -3,14 +3,19 @@ CREATE TABLE IF NOT EXISTS "Users" (
     "email" VARCHAR(255) NOT NULL UNIQUE,
     "firstName" VARCHAR(150) NOT NULL,
     "lastName" VARCHAR(150) NOT NULL,
-    "password" VARCHAR(255) NOT NULL
+    "password" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "Contents"  (
-    "contentId" INT NOT NULL PRIMARY KEY,
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "contentId" INT NOT NULL,
     title VARCHAR(100) NOT NULL,
     description TEXT,
-    "thumbnailURL" VARCHAR(500) NOT NULL,
+    "thumbnailUrl" VARCHAR(500) NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP NOT NULL,
     duration INT NULL, -- Video Class
     "channelId" INT NULL, -- Video Class
     "subscriberCount" INT NULL, -- Channel Class
@@ -22,13 +27,17 @@ CREATE TABLE IF NOT EXISTS "Tags" (
     "tagId" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     label VARCHAR(50) NOT NULL,
     "userId" INT NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP NOT NULL,
     CONSTRAINT fk_tags_user FOREIGN KEY ("userId") REFERENCES "Users"("userId")
 );
 
 CREATE TABLE IF NOT EXISTS "ContentTags" (
     "tagId" INT NOT NULL,
     "contentId" INT NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP NOT NULL,
     CONSTRAINT pk_contenttags_ids PRIMARY KEY ("tagId", "contentId"),
     CONSTRAINT fk_contenttags_tagId FOREIGN KEY ("tagId") REFERENCES "Tags"("tagId"),
-    CONSTRAINT fk_contenttags_contentId FOREIGN KEY ("contentId") REFERENCES "Contents"("contentId")
+    CONSTRAINT fk_contenttags_contentId FOREIGN KEY ("contentId") REFERENCES "Contents"("id")
 );
