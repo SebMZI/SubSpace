8a5ca786eb59548ab0aaaee741fe11d41f788426
SebMZI <sebastien.mzi@proton.me>
2026-08-11 16:49:52 +0200

    feat: create App Class that is instanciated in server.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,34 +2,59 @@ import "./config/env.config";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import {rateLimit} from "express-rate-limit";
+import {rateLimit, RateLimitRequestHandler} from "express-rate-limit";
 import {connectDatabase} from "./config/db.config";
 import "./models/associations";
 import API_ROUTES from "./routes/api.routes";
 
-const APP = express();
-const PORT = process.env.PORT || 3000;
+export class App {
+    app: express.Application;
+    port: number = Number(process.env.PORT) || 3000;
 
-const GLOBAL_LIMITER = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 300,
-    standardHeaders: "draft-8",
-    legacyHeaders: false,
-});
+    constructor() {
+        this.app = express();
+    }
 
-APP.use(cors());
-APP.use(helmet());
-APP.use(GLOBAL_LIMITER);
-APP.use(express.json());
+    async initialize() {
+        this.setDefaultMiddlewares();
+        this.setRateLimit();
+        this.setRoutes();
+        this.setNotFoundPath();
+        await this.startServer();
+    }
 
-// ROUTES
-APP.use("/api/v1", API_ROUTES);
+    private setRateLimit(): RateLimitRequestHandler {
+        return rateLimit({
+            windowMs: 10 * 60 * 1000,
+            limit: 300,
+            standardHeaders: "draft-8",
+            legacyHeaders: false,
+        });
+    }
 
-APP.use((req: any, res: any)=> {
-    res.status(404).send("Not Found");
-})
-// SERVER
-APP.listen(PORT, async () => {
-    console.log(`Server is running on http://localhost:${PORT}/api/v1`);
-    await connectDatabase();
-});
+    private setDefaultMiddlewares() {
+        const RATE_LIMIT = this.setRateLimit();
+
+        this.app.use(cors());
+        this.app.use(helmet());
+        this.app.use(RATE_LIMIT);
+        this.app.use(express.json());
+    }
+
+    private setRoutes() {
+        this.app.use("/api/v1", API_ROUTES);
+    }
+
+    private setNotFoundPath() {
+        this.app.use((req: any, res: any)=> {
+            res.status(404).send("Not Found");
+        })
+    }
+
+    private async startServer() {
+        this.app.listen(this.port, async () => {
+            console.log(`Server is running on http://localhost:${this.port}/api/v1`);
+            await connectDatabase();
+        });
+    }
+}
